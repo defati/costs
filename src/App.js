9c99922717824c1798e275eda70e49edf558ae6f
Eduardo Ferreira <eduardo.defati@gmail.com>
2022-02-04 22:13:34 -0300

    layout ok e pagina home

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "./components/layout/Container";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import Company from "./components/pages/Company";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+import NavBar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
-      <Routes>
-        <Container customClass="min-height">
-          <Route path="/" element={<Home/>}/>
-          <Route path="/company" element={<Company/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/newproject" element={<NewProject/>}/>
-        </Container>
-      </Routes>
-      <p>Footer</p>
+      <NavBar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
-  )
+  );
 }
 
 export default App;
